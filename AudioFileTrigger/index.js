daa171c1b09d9a535c7734cc12fb0141d68b6208
Ray Kao <ray@peopleandcode.com>
2018-03-24 12:10:58 -0400

    testing error without myBlob ref

--- AudioFileTrigger/index.js
+++ AudioFileTrigger/index.js
@@ -1,4 +1,4 @@
-module.exports = function (context, myBlob) {
-  context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+module.exports = function (context) {
+  context.log("JavaScript blob trigger function processed blob \n Name:");
   context.done();
 };
